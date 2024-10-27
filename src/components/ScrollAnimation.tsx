1f141d58a22865146d42fa5d0999aadd1361cd1a
HoCG <rhwkd5012@naver.com>
2024-10-27 17:28:57 +0900

    feat: 카드처럼 튀어나오는 프로모션

--- src/components/ScrollAnimation.tsx
+++ src/components/ScrollAnimation.tsx
@@ -1,14 +1,38 @@
-import { motion, MotionValue, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import React from "react";
 import styled from "styled-components";
+
 export default function ScrollAnimation() {
   const { scrollYProgress } = useScroll();
+  const [isHovered, setIsHovered] = React.useState(false);
   const y1 = useTransform(scrollYProgress, [0, 1], [0, -3000]);
   const y2 = useTransform(scrollYProgress, [0, 0.5], [1400, 2000]);
   const y3 = useTransform(scrollYProgress, [0, -6000], [20000, 23000]);
 
   return (
     <S.ScrollAnimation>
-      test
+      <motion.div
+        className="animate-hover-card"
+        onHoverStart={() => setIsHovered(true)}
+        onHoverEnd={() => setIsHovered(false)}
+      >
+        <motion.div
+          className="animate-hover-card--first"
+          style={{ transformOrigin: "right bottom" }}
+          transition={{
+            duration: 0.3,
+          }}
+          animate={
+            isHovered
+              ? { rotate: [20, 50, 20], zIndex: 3 }
+              : { rotate: [20, 51, 20] }
+          }
+        />
+        <motion.div
+          className="animate-hover-card--second"
+          animate={isHovered ? {} : {}}
+        />
+      </motion.div>
       <motion.div style={{ y: y1 }} className="animate-scroll-background" />
       <motion.div style={{ y: y2 }} className="animate-scroll-block" />
       <motion.div style={{ y: y3, x: 200 }} className="animate-scroll-block" />
@@ -31,6 +55,25 @@ const S = {
       width: 200px;
       background-color: #11442b;
       height: 200px;
+    }
+    .animate-hover-card {
+      position: absolute;
+      left: 50%;
+      top: 20000px;
+      &--first {
+        position: absolute;
+        width: 200px;
+        height: 300px;
+        background-color: #ff9421;
+        border-radius: 20px;
+      }
+      &--second {
+        position: absolute;
+        width: 200px;
+        height: 300px;
+        background-color: #8cff21;
+        border-radius: 20px;
+      }
     }
   `,
 };
